92f6115541a44e3970e032133c189a68040de6c9
Elena Liubimova <elena.liubimova3@gmail.com>
2023-02-11 23:51:40 +0300

    feat: начать валидацию

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,13 +1,18 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import PopupWithForm from "./PopupWithForm";
 
 const EditAvatarPopup = ({ onUpdateAvatar, isOpen, onClose }) => {
   const inputRef = useRef();
+  const [error, setError] = useState("");
 
   function handleSubmit(evt) {
     evt.preventDefault();
 
     onUpdateAvatar({ avatar: inputRef.current.value });
+  }
+
+  function handleChange() {
+    setError(inputRef.current.validationMessage);
   }
 
   return (
@@ -21,15 +26,22 @@ const EditAvatarPopup = ({ onUpdateAvatar, isOpen, onClose }) => {
     >
       <label className="edit-form__field">
         <input
-          className="edit-form__item"
+          className={
+            error
+              ? "edit-form__item edit-form__item_type_error"
+              : "edit-form__item"
+          }
           ref={inputRef}
+          onChange={handleChange}
           id="avatar-input"
           type="url"
           name="avatar"
           placeholder="Ссылка на аватар"
           required
         />
-        <span className="edit-form__item-error avatar-input-error"></span>
+        <span className="edit-form__item-error avatar-input-error">
+          {error}
+        </span>
       </label>
     </PopupWithForm>
   );
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -6,6 +6,8 @@ const EditProfilePopup = ({ onUpdateUser, isOpen, onClose }) => {
   const { currentUser } = useContext(CurrentUserContext);
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
+  const [nameInputError, setNameInputError] = useState("");
+  const [descriptionInputError, setDescriptionInputError] = useState("");
 
   useEffect(() => {
     setName(currentUser.name);
@@ -14,10 +16,12 @@ const EditProfilePopup = ({ onUpdateUser, isOpen, onClose }) => {
 
   function handleNameInput(evt) {
     setName(evt.target.value);
+    setNameInputError(evt.target.validationMessage);
   }
 
   function handleDescriptionInput(evt) {
     setDescription(evt.target.value);
+    setDescriptionInputError(evt.target.validationMessage);
   }
 
   function handleSubmit(evt) {
@@ -40,7 +44,11 @@ const EditProfilePopup = ({ onUpdateUser, isOpen, onClose }) => {
     >
       <label className="edit-form__field">
         <input
-          className="edit-form__item"
+          className={
+            nameInputError
+              ? "edit-form__item edit-form__item_type_error"
+              : "edit-form__item"
+          }
           id="name-input"
           value={name || ""}
           onChange={handleNameInput}
@@ -51,11 +59,15 @@ const EditProfilePopup = ({ onUpdateUser, isOpen, onClose }) => {
           maxLength="40"
           required
         />
-        <span className="edit-form__item-error name-input-error"></span>
+        <span className="edit-form__item-error name-input-error">{nameInputError}</span>
       </label>
       <label className="edit-form__field">
         <input
-          className="edit-form__item"
+          className={
+            descriptionInputError
+              ? "edit-form__item edit-form__item_type_error"
+              : "edit-form__item"
+          }
           id="job-input"
           type="text"
           name="job"
@@ -66,7 +78,7 @@ const EditProfilePopup = ({ onUpdateUser, isOpen, onClose }) => {
           maxLength="200"
           required
         />
-        <span className="edit-form__item-error job-input-error"></span>
+        <span className="edit-form__item-error job-input-error">{descriptionInputError}</span>
       </label>
     </PopupWithForm>
   );
